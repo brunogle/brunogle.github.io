9b041ba1cbe5b1a3170e3df2e483983e0751e1f4
Bruno Glecer <bruno.glecer@gmail.com>
2025-12-11 15:11:24 -0300

    Some projects, basic template

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("images");
+
+  eleventyConfig.addPassthroughCopy("posts/1-em-simulator/images");
+  eleventyConfig.addPassthroughCopy("posts/1-em-simulator/js");
+  eleventyConfig.addPassthroughCopy("posts/1-em-simulator/styles.css");
+
 };
